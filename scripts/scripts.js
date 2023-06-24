0274e18a45917462a76b18d0869b6f41a6edd67d
Mel <melissabrito2015@outlook.com.br>
2023-06-24 14:50:55 -0300

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,79 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+//clap
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+// tim 
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+// puff 
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+// splash 
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+// toim 
+
+function tocaSomtoim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+
+document.querySelector('.tecla_toim').onclick = tocaSomtoim;
+
+// psh
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+// tic
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+// tom
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+
